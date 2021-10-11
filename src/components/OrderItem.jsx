13ef0f4f890ef4b83feb4215d1727334260642ec
Javiera Fortunato <javiera.fortunato@gmail.com>
2021-10-11 10:57:39 -0300

    almost all components created, only remains ProductItem

---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import '../styles/OrderItem.scss'
+
+const OrderItem = () => {
+  return (
+    <div className="OrderItem">
+      <div className="shopping-cart">
+        <figure>
+          <img
+            src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            alt="bike"
+          ></img>
+        </figure>
+        <p>Bike</p>
+        <p>$30,00</p>
+      </div>
+    </div>
+  );
+};
+
+export default OrderItem;
